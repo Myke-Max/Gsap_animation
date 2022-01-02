c651aebd2c3811cbc402a0b533036be1a880b73b
Mike-Max <scartman1992@gmail.com>
2022-01-02 20:59:22 +0000

    prepare local work and start project

---
+++ gsap_animation.js
@@ -0,0 +1,5 @@
+gsap.to('.cup', { duration: 3, y: '0%' });
+gsap.to('.confetie', { duration: 3, delay: 1, y: '0%' });
+gsap.to('.funs_right', { duration: 3, delay: 3, x: '0%', y: '0%' });
+gsap.to('.funs_left', { duration: 3, delay: 3, x: '0%', y: '0%' });
+gsap.to('.title_one', { duration: 3, delay: 4, opacity: 1 });
