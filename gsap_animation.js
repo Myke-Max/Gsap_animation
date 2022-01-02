c793b2b1d4e2b8b763a73bad392b0ef3486c0278
Mike-Max <scartman1992@gmail.com>
2022-01-02 23:51:12 +0000

    without last slide

--- gsap_animation.js
+++ gsap_animation.js
@@ -1,5 +1,48 @@
 gsap.to('.cup', { duration: 3, y: '0%' });
-gsap.to('.confetie', { duration: 3, delay: 1, y: '0%' });
-gsap.to('.funs_right', { duration: 3, delay: 3, x: '0%', y: '0%' });
-gsap.to('.funs_left', { duration: 3, delay: 3, x: '0%', y: '0%' });
-gsap.to('.title_one', { duration: 3, delay: 4, opacity: 1 });
+gsap.to('.confetti', { duration: 3, delay: 1, y: '0%' });
+gsap.to('.funs_left', { duration: 3, delay: 3, x: '-1%' });
+gsap.to('.funs_right', { duration: 3, delay: 3, x: '1%' });
+
+gsap.to('.title_one', { duration: 3, delay: 5, opacity: 1 });
+gsap.to('.team_left', {
+  duration: 4,
+  delay: 6,
+  keyframes: [
+    { opacity: 0.5 },
+    { opacity: 0.7 },
+    { opacity: 1 },
+    { opacity: 0.7 },
+    { opacity: 0.5 },
+    { opacity: 0 },
+  ],
+});
+gsap.to('.team_right', {
+  duration: 4,
+  delay: 6,
+  keyframes: [
+    { opacity: 0.5 },
+    { opacity: 0.7 },
+    { opacity: 1 },
+    { opacity: 0.7 },
+    { opacity: 0.5 },
+    { opacity: 0.2 },
+    { opacity: 0 },
+  ],
+});
+
+gsap.to('.label_left', {
+  duration: 3,
+  delay: 10,
+  keyframes: [{ opacity: 0.2 }, { opacity: 0.5 }, { opacity: 0.7 }, { opacity: 1 }],
+});
+gsap.to('.label_right', {
+  duration: 3,
+  delay: 10,
+  keyframes: [{ opacity: 0.2 }, { opacity: 0.5 }, { opacity: 0.7 }, { opacity: 1 }],
+});
+
+// gsap.to('.final_result', {
+//   duration: 3,
+//   delay: 10,
+//   keyframes: [{ opacity: 0.2 }, { opacity: 0.5 }, { opacity: 0.7 }, { opacity: 1 }],
+// });
