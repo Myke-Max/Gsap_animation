e8376dcdaa04dd1ab0e113c87131fd351a1c2cd9
Mike-Max <scartman1992@gmail.com>
2022-01-03 17:38:34 +0000

    work , but need to refactor details

--- gsap_animation.js
+++ gsap_animation.js
@@ -1,9 +1,56 @@
 gsap.to('.cup', { duration: 3, y: '0%' });
 gsap.to('.confetti', { duration: 3, delay: 1, y: '0%' });
-gsap.to('.funs_left', { duration: 3, delay: 3, x: '-1%' });
-gsap.to('.funs_right', { duration: 3, delay: 3, x: '1%' });
+gsap.to('.funs_left', {
+  duration: 14,
+  delay: 3,
+  keyframes: [
+    { x: '-60%' },
+    { x: '-40%' },
+    { x: '-20%' },
+    { x: '-1%' },
+    { x: '-1%' },
+    { x: '-1%' },
+    { x: '-1%' },
+    { x: '-1%' },
+    { x: '-1%' },
+    { x: '-20%' },
+    { x: '-40%' },
+    { x: '-60%' },
+    { x: '-80%' },
+  ],
+});
+gsap.to('.funs_right', {
+  duration: 14,
+  delay: 3,
+  keyframes: [
+    { x: '60%' },
+    { x: '40%' },
+    { x: '20%' },
+    { x: '1%' },
+    { x: '1%' },
+    { x: '1%' },
+    { x: '1%' },
+    { x: '1%' },
+    { x: '1%' },
+    { x: '20%' },
+    { x: '40%' },
+    { x: '60%' },
+    { x: '80%' },
+  ],
+});
 
-gsap.to('.title_one', { duration: 3, delay: 5, opacity: 1 });
+gsap.to('.title_one', {
+  duration: 7,
+  delay: 5,
+  keyframes: [
+    { opacity: 0.5 },
+    { opacity: 0.7 },
+    { opacity: 1 },
+    { opacity: 0.7 },
+    { opacity: 0.5 },
+    { opacity: 0 },
+  ],
+});
 gsap.to('.team_left', {
   duration: 4,
   delay: 6,
@@ -25,24 +72,39 @@ gsap.to('.team_right', {
     { opacity: 1 },
     { opacity: 0.7 },
     { opacity: 0.5 },
-    { opacity: 0.2 },
     { opacity: 0 },
   ],
 });
 
 gsap.to('.label_left', {
-  duration: 3,
+  duration: 4,
   delay: 10,
-  keyframes: [{ opacity: 0.2 }, { opacity: 0.5 }, { opacity: 0.7 }, { opacity: 1 }],
+  keyframes: [
+    { opacity: 0.2 },
+    { opacity: 0.5 },
+    { opacity: 0.7 },
+    { opacity: 1 },
+    { x: '-10%' },
+    { x: '-20%' },
+    { x: '-30%' },
+  ],
 });
 gsap.to('.label_right', {
-  duration: 3,
+  duration: 4,
   delay: 10,
-  keyframes: [{ opacity: 0.2 }, { opacity: 0.5 }, { opacity: 0.7 }, { opacity: 1 }],
+  keyframes: [
+    { opacity: 0.2 },
+    { opacity: 0.5 },
+    { opacity: 0.7 },
+    { opacity: 1 },
+    { x: '10%' },
+    { x: '20%' },
+    { x: '30%' },
+  ],
 });
 
-// gsap.to('.final_result', {
-//   duration: 3,
-//   delay: 10,
-//   keyframes: [{ opacity: 0.2 }, { opacity: 0.5 }, { opacity: 0.7 }, { opacity: 1 }],
-// });
+gsap.to('.final_result', {
+  opacity: 1,
+  duration: 1,
+  delay: 12,
+});
