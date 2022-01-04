1135d97a5a630bfe7d17f47a7ee450ff78514089
Mike-Max <scartman1992@gmail.com>
2022-01-04 00:56:27 +0000

    add modal and frame for video content

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,40 @@
-import * as basicLightbox from 'basiclightbox';
-import 'basiclightbox/src/styles/main.scss';
-
+const refs = {
+  openModal: document.querySelector('.js-lightbox'),
+  backdrop: document.querySelector('.lightbox__overlay'),
+  iframe: document.querySelector('.iframe'),
+  modalCloseBtn: document.querySelector('.lightbox__button'),
+  linkButton: document.querySelector('.content_button'),
+  modal_animation: document.querySelector('.modal'),
+  close_button: document.querySelector('.close_button'),
+};
 export default function openModal(e) {
   if (e.target.nodeName !== 'BUTTON') {
     return;
-  } else if (e.code === 'Escape') {
-    return;
   }
-  basicLightbox
-    .create(
-      `  <iframe width="560" height="315" src='https://myke-max.github.io/goit-js-hw-13-image-finder/'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
-    )
-    .show();
+  e.preventDefault();
+  refs.openModal.classList.add('is-open');
+  refs.modalCloseBtn.addEventListener('click', onCloseModal);
+  refs.backdrop.addEventListener('click', onBackDropClose);
+  window.addEventListener('keydown', onEscClose);
 }
+const onCloseModal = () => {
+  refs.openModal.classList.remove('is-open');
+  refs.iframe.src = '';
+};
+const onEscClose = e => {
+  if (e.code === 'Escape') {
+    onCloseModal();
+  }
+};
+
+const onBackDropClose = e => {
+  if (e.currentTarget === e.target) {
+    onCloseModal();
+  }
+};
+
+const onCloseAnimation = () => {
+  refs.modal_animation.classList.add('close');
+};
+refs.linkButton.addEventListener('click', openModal);
+refs.close_button.addEventListener('click', onCloseAnimation);
